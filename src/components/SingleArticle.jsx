f19c20592aeb6c921fb240a18ebdb1378eeb4e95
waiKin <mou@waiKins-MacBook-Pro.local>
2023-06-02 11:20:21 +0100

    task7

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,35 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { fetchArticleById, getArticleComments, voteArticle } from "../../utils";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function SingleArticle() {
   const [article, setArticle] = useState("");
   let { articleId } = useParams();
 
   const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
+
   const handleViewComments = () => {
-    if (comments.length === 0) {
-      getArticleComments(articleId)
-        .then((comments) => {
-          setComments(comments);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      setComments([]);
-    }
+    setShowComments(!showComments);
   };
 
   const handleVote = () => {
-    voteArticle(articleId)
-      .then((vote) => {
-        setArticle(vote);
+    voteArticle(articleId, 1);
+  };
+
+  useEffect(() => {
+    getArticleComments(articleId)
+      .then((comments) => {
+        setComments(comments);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
+  }, []);
 
   useEffect(() => {
     fetchArticleById(+articleId)
@@ -39,34 +35,32 @@ function SingleArticle() {
       .then((article) => {
         setArticle(article);
       });
-  }, []);
+  }, [article]);
 
   return (
     <div className="single-article">
       <h2 className="article-title">{article.title}</h2>
       <p className="article-author">Author: {article.author}</p>
-
       <img
         className="article-image"
-        src={article.article_img_url}
+        src={article.articleimgurl}
         alt="Article"
       />
       <p className="article-body">{article.body}</p>
-
-      <button onClick={handleVote}>Vote Here👍🏻 </button>
       <p>{article.votes} votes</p>
+      <button onClick={() => handleVote()}>Vote</button>
 
-      <button onClick={handleViewComments}>
-        {comments.length === 0 ? "View Comments" : "Hide Comments"}
+      <button onClick={() => handleViewComments()}>
+        {showComments ? "Hide Comments" : "View Comments"}
       </button>
-
-      {comments.map((comment) => (
-        <div key={comment.id} className="comment-card">
-          <p>{comment.author}</p>
-          <p>posted on {comment.created_at}</p>
-          <p>{comment.body}</p>
-        </div>
-      ))}
+      {showComments &&
+        comments.map((comment, index) => (
+          <div key={index} className="comment-card">
+            <p>{comment.author}</p>
+            <p>posted on {comment.created_at}</p>
+            <p>{comment.body}</p>
+          </div>
+        ))}
     </div>
   );
 }
